af02a074fd971110e52b7abc58cf40899eeaabf5
Paulo Júnior <pauloafpjunior@hotmail.com>
2017-10-06 11:20:14 -0300

    fixes a pagination isse

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017061741;         // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017061742;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;         // Requires this Moodle version
 $plugin->component = 'report_coursestats'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
