cd74565422d50a07fae3964c5d776f18301c753e
Paulo Júnior <pauloafpjunior@hotmail.com>
2017-10-06 10:39:15 -0300

    update readme and translation file

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Hence, we developed "Courses Usage Statistics"; this is a Moodle report plugin t
 
 ## Releases
 
-- v2.2: this version upgrades the front-end of the main page. 
+- v2.2: this version upgrades the front-end of the main page and the translation file. 
 - v2.1: this version fixes an issue related to plugin upgrade. 
 - v2.0: this version allows the user filtering data based on course categories. 
 - v1.3: this version fixes an issue related to division by zero error and improves the documentation about the plugin. 
--- lang/pt_br/report_coursestats.php
+++ lang/pt_br/report_coursestats.php
@@ -35,13 +35,13 @@ $string['lb_all_categories'] = 'Todas';
 $string['lb_null_usage'] = 'Nenhum';
 $string['lb_forum_usage'] = 'Apenas fórum';
 $string['lb_courses_amount'] = 'Total de cursos';
-$string['lb_forum_usage_help'] = 'O curso está sendo usado apenas para comunicação entre professor e alunos, por meio do <b>Fórum de Notícias</b>.';
+$string['lb_forum_usage_help'] = 'Curso que estão sendo utilizados apenas para comunicação entre professor e alunos, por meio do <b>Fórum de Notícias</b>.';
 $string['lb_repository_usage'] = 'Apenas repositório';
-$string['lb_repository_usage_help'] = 'O curso está sendo usado apenas como um repositório de arquivos. Ou seja, o professor disponibilizou 
-  apenas <b>arquivos</b>, <b>pasta</b> ou <b>URLs</b> neste curso.';
+$string['lb_repository_usage_help'] = 'Cursos que estão sendo utilizados apenas como repositórios de arquivos. Ou seja, o professor disponibilizou 
+  apenas <b>arquivos</b>, <b>pasta</b> ou <b>URLs</b> nestes cursos.';
 $string['lb_activity_usage'] = 'Atividades';
-$string['lb_activity_usage_help'] = 'O curso está sendo efetivamente usado. Ou seja, o professor criou tipos de módulos interessantes
-  tais como <b>Fórum</b>, <b>Questionário</b>, <b>Quiz</b>, <b>Tarefas</b>, entre outros neste curso.';
+$string['lb_activity_usage_help'] = 'Curso que estão sendo efetivamente utilizados. Ou seja, o professor criou tipos de módulos interessantes
+  tais como <b>Fórum</b>, <b>Questionário</b>, <b>Quiz</b>, <b>Tarefas</b>, entre outros nestes cursos.';
 $string['lb_chart_series'] = 'Estatísticas de uso de cursos';
 $string['lb_there_are_no_courses_stats'] = 'Não há estatísticas de uso a serem apresentadas no momento!';
 
