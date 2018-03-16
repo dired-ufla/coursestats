9a93dd1d38a168233968a9d1217410784ebb3ae2
Paulo <pauloafpjunior@hotmail.com>
2018-03-16 14:09:51 -0300

    chart departments

--- departments.php
+++ departments.php
@@ -83,6 +83,10 @@ $departments = array(
 	array('cod'=>'gzo', 'acr'=>'DZO', 'name'=>'DZO - Zootecnia')
 ); 
 
+$dept_acr_array = array('DAG', 'DBI', 'DCA', 'DCC', 'DCS', 'DSA', 'DEX', 
+	'DCF', 'DCH', 'DIR', 'DED', 'DEG', 'DEB', 'DES', 'DFI', 'DFP', 
+	'DNU', 'DMV', 'DQI', 'DZO');
+
 $url = new moodle_url($CFG->wwwroot . '/report/coursestats/index.php');
 $link = html_writer::link($url, get_string('link_back', 'report_coursestats'));
 
@@ -95,11 +99,6 @@ if ($category == ALL_CATEGORIES) {
 
 echo $OUTPUT->header();
 echo $OUTPUT->heading(get_string('pluginname', 'report_coursestats') . ' (' .$catname. ') - ' . $link);
-
-$url_csv = new moodle_url($CFG->wwwroot . '/report/coursestats/csvgen.php?category=' . $category);
-$link_csv = html_writer::link($url_csv, get_string('link_csv', 'report_coursestats'));
-
-echo '<p align="center">' . $link_csv . '</p>';
 
 $table = new html_table();
 $table->size = array( '55%', '15%', '15%', '15%');
@@ -117,21 +116,56 @@ if ($co_created > 0) {
 	
 $table->data[] = array($link, $co_created, $co_used, $co_percent);
 
+$created_courses_array = array();
+$used_courses_array = array();
+$percentage_used_courses_array = array();
+
 foreach ($departments as $depto) {
 	$co_created = get_amount_created_courses($depto['cod']);
 	$co_used = get_amount_used_courses($depto['cod']);
+	
+	$created_courses_array[] = $co_created;
+	$used_courses_array[] = $co_used;
 	
 	$link = '<a href=' . $CFG->wwwroot . '/report/coursestats/main.php?category=' . $category . '&depname=' . $depto['acr'] . 
 		'&dep=' . $depto['cod'] . '>' . $depto['name'] . '</a>';
 	
 	if ($co_created > 0) {
 		$co_percent = number_format(($co_used / $co_created) * 100, 2) . '%';
+		$percentage_used_courses_array[] = number_format(($co_used / $co_created) * 100, 2);
 	} else {
 		$co_percent = '-';
+		$percentage_used_courses_array[] = 0;
 	}
 	$table->data[] = array($link, $co_created, $co_used, $co_percent);
 }
  
+if (class_exists('core\chart_bar')) {
+	$chart_stacked = new core\chart_bar();
+	$chart_percentage = new core\chart_bar();
+	
+	$chart_stacked->set_stacked(true);
+	
+	$created_courses_serie = new core\chart_series(get_string('lb_courses_created_amount', 'report_coursestats'), $created_courses_array);
+	$used_courses_serie = new core\chart_series(get_string('lb_used_courses', 'report_coursestats'), $used_courses_array);
+	$percentage_used_courses_serie = new core\chart_series(get_string('lb_percent_of_used_courses', 'report_coursestats'), $percentage_used_courses_array);
+	
+	$chart_stacked->add_series($created_courses_serie);
+	$chart_stacked->add_series($used_courses_serie);
+	$chart_stacked->set_labels($dept_acr_array);
+
+	$chart_percentage->add_series($percentage_used_courses_serie);
+	$chart_percentage->set_labels($dept_acr_array);
+	
+	echo $OUTPUT->render_chart($chart_stacked, false);
+	echo $OUTPUT->render_chart($chart_percentage, false);
+}
+
+$url_csv = new moodle_url($CFG->wwwroot . '/report/coursestats/csvgen.php?category=' . $category);
+$link_csv = html_writer::link($url_csv, get_string('link_csv', 'report_coursestats'));
+
+echo '<p align="center">' . $link_csv . '</p>';
+
 echo html_writer::table($table);
 
 echo $OUTPUT->footer();
